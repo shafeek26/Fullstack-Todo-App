2d6cc5a4e252050e19ae7421bf5af4ab3a2bf7d3
shafiq <makshafiq588@gmail.com>
2022-12-25 22:57:58 +0530

    code structured

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,51 +1,55 @@
-const Todo = require('../model/todoModel');
-const asyncHandler = require('express-async-handler');
+/** @format */
+
+const Todo = require("../model/todoModel");
+const asyncHandler = require("express-async-handler");
 
 // @desc fetching todo
 // @route GET /todo/getTodo
-const getTodo = asyncHandler( async (req, res) => {
-    const todo = await Todo.find()
-    res.status(200).json(todo)
-})
+const getTodo = asyncHandler(async (req, res) => {
+  const todo = await Todo.find();
+  res.status(200).json(todo);
+});
 // @desc adding todo
 // @route POST /todo/addTodo
-const addTodo = asyncHandler( async (req, res) => {
-    const { todo } = req.body
-    
-    if(!todo){
-        res.status(400).json('Todo field is required')
-    }
+const addTodo = asyncHandler(async (req, res) => {
+  const { todo } = req.body;
 
-    const newTodo = await Todo.create({ todo })
-    res.status(200).json(newTodo)
-})
+  if (!todo) {
+    res.status(400).json("Todo field is required");
+  }
+
+  const newTodo = await Todo.create({ todo });
+  res.status(200).json(newTodo);
+});
 // @desc updating todo
 // @route PUT /todo/updateTodo
-const updateTodo = asyncHandler( async (req, res) => {
-    const todo = await Todo.findById(req.params.id);
-    if(!todo){
-        res.status(400).json('Todo not found')
-    }
+const updateTodo = asyncHandler(async (req, res) => {
+  const todo = await Todo.findById(req.params.id);
+  if (!todo) {
+    res.status(400).json("Todo not found");
+  }
 
-    const updatedTodo = await Todo.findByIdAndUpdate(todo._id, req.body, { new : true})
-    res.status(200).json(updatedTodo)
-})
+  const updatedTodo = await Todo.findByIdAndUpdate(todo._id, req.body, {
+    new: true,
+  });
+  res.status(200).json(updatedTodo);
+});
 // @desc fetching todo
 // @route DELETE /todo/deleteTodo
-const deleteTodo = asyncHandler( async (req, res) => {
-    const todo = await Todo.findById(req.params.id);
+const deleteTodo = asyncHandler(async (req, res) => {
+  const todo = await Todo.findById(req.params.id);
 
-    if(!todo){
-        res.status(400).json('There is no todo to delete')
-    }
-   
-    await todo.remove()
-    res.status(200).json({id : req.params.id})
-})
+  if (!todo) {
+    res.status(400).json("There is no todo to delete");
+  }
+
+  await todo.remove();
+  res.status(200).json({ id: req.params.id });
+});
 
 module.exports = {
-    getTodo,
-    addTodo,
-    updateTodo,
-    deleteTodo
-}
+  getTodo,
+  addTodo,
+  updateTodo,
+  deleteTodo,
+};
