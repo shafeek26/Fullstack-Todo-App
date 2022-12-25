421ab4d0b07b2812b3bf753a77bbcaf1ef05ddd2
shafiq <makshafiq588@gmail.com>
2022-12-25 22:56:07 +0530

    CRUD functionility added

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const router = require('./routes/todoRoute')
 
+app.use(express.json());
+app.use(express.urlencoded({extended : true}))
 app.use('/api/todo', router)
 
 module.exports = app;
--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,23 +1,47 @@
+const Todo = require('../model/todoModel');
+const asyncHandler = require('express-async-handler');
+
 // @desc fetching todo
 // @route GET /todo/getTodo
-const getTodo = (req, res) => {
-    res.status(200).json({message : 'get todo'})
-}
+const getTodo = asyncHandler( async (req, res) => {
+    const todo = await Todo.find()
+    res.status(200).json(todo)
+})
 // @desc adding todo
 // @route POST /todo/addTodo
-const addTodo = (req, res) => {
-    res.status(200).json({message : 'Add todo'})
-}
+const addTodo = asyncHandler( async (req, res) => {
+    const { todo } = req.body
+    
+    if(!todo){
+        res.status(400).json('Todo field is required')
+    }
+
+    const newTodo = await Todo.create({ todo })
+    res.status(200).json(newTodo)
+})
 // @desc updating todo
 // @route PUT /todo/updateTodo
-const updateTodo = (req, res) => {
-    res.status(200).json({message : `updated todo ${req.params.id}`})
-}
+const updateTodo = asyncHandler( async (req, res) => {
+    const todo = await Todo.findById(req.params.id);
+    if(!todo){
+        res.status(400).json('Todo not found')
+    }
+
+    const updatedTodo = await Todo.findByIdAndUpdate(todo._id, req.body, { new : true})
+    res.status(200).json(updatedTodo)
+})
 // @desc fetching todo
 // @route DELETE /todo/deleteTodo
-const deleteTodo = (req, res) => {
-    res.status(200).json({message : `deleted todo ${req.params.id}`})
-}
+const deleteTodo = asyncHandler( async (req, res) => {
+    const todo = await Todo.findById(req.params.id);
+
+    if(!todo){
+        res.status(400).json('There is no todo to delete')
+    }
+   
+    await todo.remove()
+    res.status(200).json({id : req.params.id})
+})
 
 module.exports = {
     getTodo,
